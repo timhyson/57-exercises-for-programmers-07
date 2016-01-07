dfec84241244a1e8777cbc6dbb6e7acd8d4752ab
Tim Hyson <tjhyson@gmail.com>
2016-01-07 18:53:52 +0000

    recap user input

--- ruby/lib/room_area.rb
+++ ruby/lib/room_area.rb
@@ -1,6 +1,26 @@
 class RoomArea
+  attr_writer :length_in_feet
+
+  def run_app
+    prompt_length
+    prompt_width
+    recap_message
+  end
+
   def prompt_length
     puts "What is the length of the room in feet?"
     length_in_feet = gets.to_s.chomp
+    @length_in_feet = length_in_feet.to_i
   end
+
+  def prompt_width
+    puts "What is the width of the room in feet?"
+    width_in_feet = gets.to_s.chomp
+    @width_in_feet = width_in_feet.to_i
+  end
+
+  def recap_message
+   "You entered dimensions of #{@length_in_feet.to_s} feet by #{@width_in_feet.to_s} feet."
+  end
+
 end
--- ruby/spec/room_area_spec.rb
+++ ruby/spec/room_area_spec.rb
@@ -1,14 +1,29 @@
 require 'room_area'
 
 describe RoomArea do
-  describe '#prompt_length' do
-    it 'asks user for the room length' do
-      allow(subject).to receive(:gets).and_return('15')
-      expect(subject.prompt_length).to eq '15'
+  describe "#prompt_length" do
+    it "asks user for the room length" do
+      allow(subject).to receive(:gets).and_return("15")
+      expect(subject.prompt_length).to eq 15
     end
   end
 
-  xit 'calculates the area of a room' do
+  describe "#prompt_width" do
+    it "asks user for the room width" do
+      allow(subject).to receive(:gets).and_return("20")
+      expect(subject.prompt_width).to eq 20
+    end
+  end
 
+  describe "#recap_mesage" do
+    it "recaps the user's inputs" do
+      room = RoomArea.new
+      allow(room).to receive(:gets).and_return("15", "20")
+      #expect(STDOUT).to receive(:puts).with("What is the length of the room in feet?")
+      #allow(STDIN).to receive(:gets).and_return("15")
+      #expect(STDOUT).to receive(:puts).with("What is the width of the room in feet?")
+      #allow(STDIN).to receive(:gets).and_return("20")
+      expect(room.run_app).to eq("You entered dimensions of 15 feet by 20 feet.")
+    end
   end
 end
